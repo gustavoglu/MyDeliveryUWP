4bbae67b76e45ca804536646efdba9265fd3b1c9
Gustavo Giroldo <gustavoglu@hotmail.com>
2017-06-26 00:08:55 -0300

    Commit Inicial

---
+++ MyDeliveryUWP/Util/Constantes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace MyDeliveryUWP.Util
+{
+    public class Constantes
+    {
+        #region
+
+        public static Thickness PADDINGDEFAULT = new Thickness(20);
+
+        public static Thickness MARGINDEFAULT = new Thickness(5);
+
+        #endregion
+    }
+}
